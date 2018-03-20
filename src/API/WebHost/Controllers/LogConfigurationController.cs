5cd9d1b0680a8a895e981df2691274504b4599b7
Daniel Cohen <dan.cohen93@gmail.com>
2018-03-20 15:15:58 +0000

    added LogConfigurationController

---
+++ src/API/WebHost/Controllers/LogConfigurationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeaverTail.API.Controllers
+{
+    [Route("api/logconfiguartion")]
+    public class LogConfigurationController : Controller
+    {
+        public object Get()
+        {
+            
+            return new
+            {
+                Hello = "Hello"
+            };
+            return HttpStatusCode.OK;
+        }
+    }
+}
