1bb94571493312a18418cdd492cf12d3d51f7068
Gavin Toon <gavin.toon@gmail.com>
2018-03-26 21:38:22 +0100

    added sample text reader + deploy profile

---
+++ src/Providers/StaticFileLogProvider/Controllers/LogController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using LazyCache;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StaticFileLogProvider.Controllers
+{
+    [Route("api/logs")]
+    public class LogController : Controller
+    {
+
+        private readonly IAppCache _cache;
+
+        public LogController(IAppCache cache)
+        {
+            _cache = cache;
+        }
+
+        // GET api/values
+        [HttpGet("{*path}")]
+        public async Task<string> Get(string path)
+        {
+            try
+            {
+                return await _cache.GetOrAddAsync($"LogController.Get-{path}",() => ReadTextAsync(path),DateTimeOffset.Now.AddSeconds(10));
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        private async Task<string> ReadTextAsync(string filePath)
+        {
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096, useAsync: true))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string text = Encoding.UTF8.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
--- src/Providers/StaticFileLogProvider/Startup.cs
+++ src/Providers/StaticFileLogProvider/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace StaticFileLogProvider
 {
@@ -24,6 +19,11 @@ namespace StaticFileLogProvider
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddLazyCache();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Static File Log Provider", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +35,12 @@ namespace StaticFileLogProvider
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Static File Log Provider V1");
+            });
+
         }
     }
 }
